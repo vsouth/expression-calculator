b337e8e108399ca82bd2d1b0e3b8f3eeb452af93
vsouth <neppchela@gmail.com>
2023-12-19 22:00:26 +0300

    fixed a typo

--- src/main/java/ru/vsouth/Main.java
+++ src/main/java/ru/vsouth/Main.java
@@ -5,6 +5,6 @@ public class Main {
         ExpressionCalculator expressionCalculator = new ExpressionCalculator();
         System.out.println("(8 + 2 * 5)/(1 + 3 * 2 - 4) = " + expressionCalculator.calculate("(8 + 2 * 5)/(1 + 3 * 2 - 4)")); // 6
         System.out.println("2 * (3 + 4) - 5 = " + expressionCalculator.calculate("(2 * (3 + 4) - 5)")); // 9
-        System.out.println("1+15*(2-1)=" + expressionCalculator.calculate("-1+15*(-1+2)")); // 16
+        System.out.println("-1+15*(-1+2)=" + expressionCalculator.calculate("-1+15*(-1+2)")); // 16
     }
 }
